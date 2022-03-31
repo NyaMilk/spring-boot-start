21d47f9ed5d1bb5efd1a69d819010acf3268c4c5
nyamilk <nasya.milk@gmail.com>
2022-03-31 22:45:59 +0300

    test

--- boot-web/src/main/java/org/example/Application.java
+++ boot-web/src/main/java/org/example/Application.java
@@ -2,24 +2,18 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-<<<<<<< HEAD
-=======
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
-
-<<<<<<< HEAD
-
->>>>>>> e345323... new commit 2 edit
-=======
->>>>>>> 9c52f29... new commit
 }
